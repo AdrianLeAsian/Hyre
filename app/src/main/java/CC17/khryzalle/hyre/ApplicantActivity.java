3d55603cc7bfa7c1a8f7d8fc4f795dbaf70fdbc8
AdrianLeAsian <158132371+AdrianLeAsian@users.noreply.github.com>
2024-12-05 02:24:38 +0800

    Added Messaging feature working post feature profiles page added

--- app/src/main/java/CC17/khryzalle/hyre/ApplicantActivity.java
+++ app/src/main/java/CC17/khryzalle/hyre/ApplicantActivity.java
@@ -1,7 +1,8 @@
 package CC17.khryzalle.hyre;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -20,6 +21,13 @@ public class ApplicantActivity extends AppCompatActivity {
 
         ImageButton backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(v -> finish());
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(ApplicantActivity.this, ApplicantDashboardActivity.class);
+            startActivity(intent);
+            finish(); // Close the login activity
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
--- app/src/main/java/CC17/khryzalle/hyre/RecruiterActivity.java
+++ app/src/main/java/CC17/khryzalle/hyre/RecruiterActivity.java
@@ -1,6 +1,9 @@
 package CC17.khryzalle.hyre;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,17 @@ public class RecruiterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_recruiter);
+
+        ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> finish());
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(RecruiterActivity.this, RecruiterDashboardActivity.class);
+            startActivity(intent);
+            finish(); // Close the login activity
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
